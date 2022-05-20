67030549aed35b4a8b13b4022982668db88d4809
PronCan <twojini98@gmail.com>
2022-05-20 09:23:01 +0900

    w1 backup

---
+++ week1/Clone_momstouch/app/src/main/java/moe/proncan/clone_momstouch/MainActivity.kt
@@ -0,0 +1,13 @@
+package moe.proncan.clone_momstouch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_menu_main)
+        setContentView(R.layout.activity_login)
+//        setContentView(R.layout.activity_main)
+    }
+}
