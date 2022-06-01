1d4e419e97dd686e802304c5beda9153b06a0e5c
PronCan-dev <twojini98@gmail.com>
2022-06-01 23:22:28 +0900

    add recyclerview adapter

--- week3/app/src/main/java/moe/proncan/week3/MainActivity.kt
+++ week3/app/src/main/java/moe/proncan/week3/MainActivity.kt
@@ -2,11 +2,33 @@ package moe.proncan.week3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import moe.proncan.week3.databinding.ActivityMainBinding
+
+lateinit var mainBinding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainItemAdapter: MainItemAdapter
+    val list = mutableListOf<MainItemData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        init()
+    }
+
+    private fun init() {
+        mainItemAdapter = MainItemAdapter(this)
+        mainBinding.mainRecyclerView.adapter = mainItemAdapter
+
+        list.apply {
+            add(MainItemData(img = R.drawable.main1, title = "1번상품", area = "운서동", time = "3분전"
+                , price = "35000", chatCnt = "", chatImg = R.drawable.ic_temp
+                , likeCnt = "2", likeImg = R.drawable.ic_temp ))
+            add(MainItemData(img = R.drawable.main2, title = "2번상품", area = "운서동", time = "3분전"
+                , price = "35000", chatCnt = "3", chatImg = R.drawable.ic_temp
+                , likeCnt = "0", likeImg = R.drawable.ic_temp ))
+        }
     }
 }
--- week3/app/src/main/java/moe/proncan/week3/MainItemAdapter.kt
+++ week3/app/src/main/java/moe/proncan/week3/MainItemAdapter.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
-class MainItemAdapter(private val context: Context): RecyclerView.Adapter<MainItemAdapter.ViewHolder> {
+class MainItemAdapter(private val context: Context):
+    RecyclerView.Adapter<MainItemAdapter.ViewHolder>() {
 
     // 리스트 개수
     var list = mutableListOf<MainItemData>()
@@ -23,31 +26,31 @@ class MainItemAdapter(private val context: Context): RecyclerView.Adapter<MainIt
     override fun getItemCount(): Int = list.size
 
     inner class ViewHolder(view: View):RecyclerView.ViewHolder(view) {
-        private val ivItemImg: TextView = itemView.findViewById(R.id.item_main_image)
+        private val ivItemImg: ImageView = itemView.findViewById(R.id.item_main_image)
         private val tvTitle: TextView = itemView.findViewById(R.id.item_main_title)
         private val tvArea: TextView = itemView.findViewById(R.id.item_main_area)
         private val tvTime: TextView = itemView.findViewById(R.id.item_main_time)
         private val tvPrice: TextView = itemView.findViewById(R.id.item_main_price)
         // 0이면 안보이게
         private val tvLikeCount: TextView = itemView.findViewById(R.id.item_main_chat_count)
-        private val ivLikeCount: TextView = itemView.findViewById(R.id.item_main_chat)
+        private val ivLikeCount: ImageView = itemView.findViewById(R.id.item_main_chat)
         private val tvChatCount: TextView = itemView.findViewById(R.id.item_main_like_count)
-        private val ivChatCount: TextView = itemView.findViewById(R.id.item_main_like)
+        private val ivChatCount: ImageView = itemView.findViewById(R.id.item_main_like)
 
         // 바인딩
         // 이미지 바인딩 Glide 사용
+        // 시간 LocalDateTime.now() => 현재시간 https://gdlseed.tistory.com/69
         fun _binding(item: MainItemData) {
+            Glide.with(itemView).load(item.img).into(ivItemImg)
+            tvTitle.text = item.title
+            tvArea.text = item.area
+            tvTime.text = item.time
+            tvPrice.text = item.price
 
-            ivItemImg
-            tvTitle
-            tvArea
-            tvTime
-            tvPrice
-
-            tvLikeCount
-            ivLikeCount
-            tvChatCount
-            ivChatCount
+            tvLikeCount.text = item.likeCnt
+            Glide.with(itemView).load(item.likeImg).into(ivLikeCount)
+            tvChatCount.text = item.chatCnt
+            Glide.with(itemView).load(item.chatImg).into(ivChatCount)
         }
     }
 }
