31a742f2598f9c8d16dbe2f489366c1c5ee43d6f
PronCan <twojini98@gmail.com>
2022-06-02 19:12:24 +0900

    set recyclerview
    메인바인딩 뺐다 ㅡㅡ

--- week3/app/src/main/java/moe/proncan/week3/MainActivity.kt
+++ week3/app/src/main/java/moe/proncan/week3/MainActivity.kt
@@ -1,34 +1,48 @@
 package moe.proncan.week3
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import moe.proncan.week3.databinding.ActivityMainBinding
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-lateinit var mainBinding: ActivityMainBinding
+//import moe.proncan.week3.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
-
+//    lateinit var mainBinding: ActivityMainBinding
     lateinit var mainItemAdapter: MainItemAdapter
-    val list = mutableListOf<MainItemData>()
+    private val list = mutableListOf<MainItemData>()
+    lateinit var mainRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        mainRecyclerView = findViewById(R.id.main_recycler_view)
 
         init()
     }
 
     private fun init() {
         mainItemAdapter = MainItemAdapter(this)
-        mainBinding.mainRecyclerView.adapter = mainItemAdapter
+        val linearLayoutManager: LinearLayoutManager = LinearLayoutManager(this)
+        linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
+        mainRecyclerView.layoutManager = linearLayoutManager
+        mainRecyclerView.adapter = mainItemAdapter
 
         list.apply {
             add(MainItemData(img = R.drawable.main1, title = "1번상품", area = "운서동", time = "3분전"
-                , price = "35000", chatCnt = "", chatImg = R.drawable.ic_temp
+                , price = "35,000", chatCnt = "", chatImg = R.drawable.ic_temp
                 , likeCnt = "2", likeImg = R.drawable.ic_temp ))
             add(MainItemData(img = R.drawable.main2, title = "2번상품", area = "운서동", time = "3분전"
-                , price = "35000", chatCnt = "3", chatImg = R.drawable.ic_temp
+                , price = "35,000", chatCnt = "3", chatImg = R.drawable.ic_temp
                 , likeCnt = "0", likeImg = R.drawable.ic_temp ))
+
+            mainItemAdapter.list = list
+            Log.d(TAG, "maininitfun "+ mainItemAdapter.itemCount)
+            mainItemAdapter.notifyDataSetChanged()
         }
     }
 }
